2521199f8a12ebfea58c566e06147b956f98d3b4
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-08-07 16:00:17 +1000

    Start integer handling

--- CD24Scanner.java
+++ CD24Scanner.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+// TODO:
+// keywords
+// strings
+// comments are not handled
+// listing file not created
+// floats and ints
+
 public class CD24Scanner {
     private int currentLine = 1;
     private int currentColumn = 1;
@@ -55,11 +62,30 @@ public class CD24Scanner {
                 }
                 // tokenize whatever in buffer
             }
+            System.out.println("No action for mode, alphabetic: " + mode);
         } else if (Character.isDigit(c)) {
             if (mode == Mode.IDENTIFIER) {
                 buffer.append(c);
                 return;
             }
+            if (mode == Mode.NUMEIRC) {
+                buffer.append(c);
+                return;
+            }
+            if (mode == Mode.DELIMITER) {
+                if (!buffer.isEmpty()) {
+                    // TODO Tokenize before preceeting
+                    throw new IllegalAccessError("No");
+                }
+                mode = Mode.NUMEIRC;
+                buffer.append(c);
+                return;
+            }
+            if (mode == Mode.NUMEIRC) {
+                buffer.append(c);
+                return;
+            }
+            System.out.println("No action for mode, digit: " + mode);
         } else if (CharacterTypes.isDelimiter(c)) {
             if (mode == Mode.IDENTIFIER) {
                 Token t = new Token(TokenType.TIDEN, buffer.toString(), currentLine, currentColumn);
@@ -70,11 +96,96 @@ public class CD24Scanner {
                 }
                 mode = Mode.DELIMITER;
                 return;
+            // Some of these are combined to form operators such as: <=, >=, !=, ==, +=, -=, *= /=
             } else if (mode == Mode.DELIMITER) {
-                
+                if (c == '=' && buffer.length() == 1) {
+                    // can be tokenized
+                    char lastChar = buffer.charAt(0);
+                    Token t;
+                    if (lastChar == '<') {
+                        t = new Token(TokenType.TLEQL, null, currentLine, currentColumn);
+                    } else if (lastChar == '>') {
+                        t = new Token(TokenType.TGEQL, null, currentLine, currentColumn);
+                    } else if (lastChar == '!') {
+                        t = new Token(TokenType.TNEQL, null, currentLine, currentColumn);
+                    } else if (lastChar == '=') {
+                        t = new Token(TokenType.TEQEQ, null, currentLine, currentColumn);
+                    } else if (lastChar == '+') {
+                        t = new Token(TokenType.TPLEQ, null, currentLine, currentColumn);
+                    } else if (lastChar == '-') {
+                        t = new Token(TokenType.TMNEQ, null, currentLine, currentColumn);
+                    } else if (lastChar == '*') {
+                        t = new Token(TokenType.TSTEQ, null, currentLine, currentColumn);
+                    } else if (lastChar == '/') {
+                        t = new Token(TokenType.TDVEQ, null, currentLine, currentColumn);
+                    } else {
+                        throw new IllegalArgumentException("Invalid delimiter");
+                    }
+                    buffer.delete(0, buffer.length());
+                    System.out.println(t);
+                } else if (CharacterTypes.isWhitespace(c)) {
+                    if (buffer.length() == 0) {
+                        return;
+                    }
+                    char lastChar = buffer.charAt(0);
+                    Token t;
+                    if (lastChar == ',') {
+                        t = new Token(TokenType.TCOMA, null, currentLine, currentColumn);
+                    } else if (lastChar == ';') {
+                        t = new Token(TokenType.TSEMI, null, currentLine, currentColumn);
+                    } else if (lastChar == '[') {
+                        t = new Token(TokenType.TLBRK, null, currentLine, currentColumn);
+                    } else if (lastChar == ']') {
+                        t = new Token(TokenType.TRBRK, null, currentLine, currentColumn);
+                    } else if (lastChar == '(') {
+                        t = new Token(TokenType.TLPAR, null, currentLine, currentColumn);
+                    } else if (lastChar == ')') {
+                        t = new Token(TokenType.TLBRK, null, currentLine, currentColumn);
+                    } else if (lastChar == '=') {
+                        t = new Token(TokenType.TEQUL, null, currentLine, currentColumn);
+                    } else if (lastChar == '+') {
+                        t = new Token(TokenType.TPLUS, null, currentLine, currentColumn);
+                    } else if (lastChar == '-') {
+                        t = new Token(TokenType.TMINS, null, currentLine, currentColumn);
+                    } else if (lastChar == '*') {
+                        t = new Token(TokenType.TSTAR, null, currentLine, currentColumn);
+                    } else if (lastChar == '/') {
+                        t = new Token(TokenType.TDIVD, null, currentLine, currentColumn);
+                    } else if (lastChar == '%') {
+                        t = new Token(TokenType.TPERC, null, currentLine, currentColumn);
+                    } else if (lastChar == '^') {
+                        t = new Token(TokenType.TCART, null, currentLine, currentColumn);
+                    } else if (lastChar == '<') {
+                        t = new Token(TokenType.TLESS, null, currentLine, currentColumn);
+                    } else if (lastChar == '>') {
+                        t = new Token(TokenType.TGRTR, null, currentLine, currentColumn);
+                    } else if (lastChar == ':') {
+                        t = new Token(TokenType.TCOLN, null, currentLine, currentColumn);
+                    } else if (lastChar == '.') {
+                        t = new Token(TokenType.TDOTT, null, currentLine, currentColumn);
+                    } else {
+                        throw new IllegalArgumentException("Invalid delimiter");
+                    }
+                    System.out.println(t);
+                    buffer.delete(0, buffer.length());
+                } else {
+                    buffer.append(c);
+                }
+            } else if (mode == Mode.NUMEIRC) {
+                if (c == '.') {
+                    // TODO: This does not work, we might have 11.11.11
+                    // variable for "have read dot?"
+                    if (buffer.charAt(buffer.length()-1) == '.') {
+                        // TODO Double dot, tokenize
+                        return;
+                    }
+                    buffer.append(c);
+                    return;
+                }
             }
+            System.out.println("No action for mode, delimiter: " + mode);
+            
         }
-        
 
     }
 }
--- TokenOutput.java
+++ TokenOutput.java
@@ -1,6 +1,10 @@
+import java.io.FileWriter;
+
 public class TokenOutput {
     // resets when you make a newline
     private int writtenCount;
+    private String outputPath;
+    private FileWriter writer;
 
     public void write(Token t) {
         
